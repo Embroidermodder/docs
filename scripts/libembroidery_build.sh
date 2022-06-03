8914696ff8d7ac1cf81575f6e25dfd916f9c303a
robin-swift <robinswiftart@gmail.com>
2022-06-03 14:12:23 +0100

    samples, scripts folders

---
+++ scripts/libembroidery_build.sh
@@ -0,0 +1,6 @@
+
+git clone https://github.com/Embroidermodder/libembroidery
+cd libembroidery
+cmake .
+cmake --build .
+cmake --install .
---
+++ scripts/windows_build.bat
@@ -0,0 +1,6 @@
+
+pacman -S gcc cmake git bash mingw-w64-SDL2 \
+     mingw-w64-SDL2_image mingw-w64-SDL2_ttf
+git clone https://github.com/Embroidermodder/Embroidermodder
+cd Embroidermodder
+bash build.sh
